6da7337d550c8e965cb420f76bd15ef8d54e2bc9
unknown <christian@stikman.com>
2021-04-21 19:30:36 -0700

    Added time slot, text area, and button spot under header

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 // Variable to add todays date
 var currentDay = $('#currentDay')
 
+// Function to set date on page load
 $(document).ready(function() {
     currentDay.html(moment().format('dddd, MMMM Do YYYY'));
 }, 1000);
