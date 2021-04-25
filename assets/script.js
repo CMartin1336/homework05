88efc905cef641b7399808e319a471c152d4c8c9
unknown <christian@stikman.com>
2021-04-25 09:18:12 -0700

    Adding input value back from local storage on doc load

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,11 @@
 // Variable to add todays date
 var currentDay = $('#currentDay');
 var saveBtn = $('#button-addon1');
-// var entry = $("#entry").val();
 
 // Function to set date on page load
 $(document).ready(function() {
     currentDay.html(moment().format('dddd, MMMM Do YYYY'));
+    $("#entry").val(localStorage.getItem("entryContent"));
 }, 1000);
 
 // Functions Needed
@@ -13,5 +13,3 @@ saveBtn.on("click", function() {
     var entry = $("#entry").val();
     localStorage.setItem("entryContent", JSON.stringify(entry));
 });
-
-var getEntry = localStorage.getItem("entryContent");
